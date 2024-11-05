66d4c65f3c232ad5a3ec6b767292031010cb6bbd
Marcus <moliveira@pinterest.com>
2024-11-05 09:03:18 -0300

    Improve Movie row

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,19 @@
-import Image from 'next/image';
+import { Banner } from './components/Banner';
 import Header from './components/Header';
-import { InformationCircleIcon } from '@heroicons/react/24/outline';
-import { PlayIcon } from '@heroicons/react/24/solid';
+import { MovieRow } from './components/MoviewRow';
+
 
 export default function Home() {
   return (
     <div className='relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh]'>
       <Header />
       <main className='relative pb-24 pl-4 lg:pl-16'>
-        <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
-          <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
-            <Image
-              src='/banner.png'
-              alt='The Witcher'
-              fill={true}
-              className='h-[65vh] object-cover object-top lg:h-[95vh]'
-            />
-          </div>
-
-          <h1 className='text-2xl font-bold md:text-4xl lg:text-7xl'>
-            The Witcher
-          </h1>
-          <p className='text-shadow-md max-w-ws text-sx md:max-w-lg md:text-lg lg:max-w-2xl'>
-            Geralt of Rivia, a solitary monster hunter, struggles to find his
-            place in a world where people often prove more wicked than beasts.
-          </p>
-        </div>
-
-        <div className='flex space-x-3'>
-          <button className='flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5 md:text-xl'>
-            <PlayIcon className='h-6' />
-            Play
-          </button>
-          <button className='flex cursor-pointer items-center gap-x-2 rounded bg-gray-300 px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5 md:text-xl'>
-            <InformationCircleIcon className='h-6' />
-            More Info
-          </button>
-        </div>
-        <div className='flex-col space-y-4'>
-          <div className='flex space-x-4 overflow-x-scroll'>
-            {[1, 2, 3, 4, 5].map((index) => (
-              <div className='group h-28 min-w-[200px] relative transform transition duration-200 ease-in hover:scale-110'>
-                <Image
-                  key={index}
-                  src={`/item_${index}.png`}
-                  fill={true}
-                  alt='MAID'
-                  className='rounded'
-                />
-              </div>
-            ))}
-          </div>
-        </div>
+        <Banner />
+        <MovieRow sectionTitle={'Trending now'} />
+        <MovieRow sectionTitle={'Top rated'} />
+        <MovieRow sectionTitle={'Action Movies'} />
       </main>
     </div>
   );
 }
+
