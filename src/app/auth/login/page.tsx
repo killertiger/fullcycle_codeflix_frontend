3697665dfe56bbe448280ac8b871f2b5d9af60c4
Marcus <moliveira@pinterest.com>
2024-11-07 03:08:34 -0800

    feat: :sparkles: Add login page

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+export default function Login() {
+    return <div
+    className="bg-cover bg-center bg-no-repeat text-white"
+    style={{backgroundImage: 'url(/background.jpg)'}}
+    >
+        <div className="flex min-h-screen flex-col items-center justify-center py-2">
+            <div>Página login 1</div>
+        </div>
+    </div>;
+}
