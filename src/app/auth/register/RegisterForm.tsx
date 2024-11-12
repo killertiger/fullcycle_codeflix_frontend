fe26fc1880a01ce54990123decd2ce82164befd2
Marcus <moliveira@pinterest.com>
2024-11-11 16:44:06 -0800

    refactor: :recycle: Refactoring AuthForm

--- src/app/auth/register/RegisterForm.tsx
+++ src/app/auth/register/RegisterForm.tsx
@@ -2,14 +2,13 @@
 import { AuthForm } from "@/app/components/AuthForm";
 import { InputField } from "../../components/InputField";
 
-export default function LoginForm() {
+export default function RegisterForm() {
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log('submit');
+        console.log('submit from register');
     }
 
     return (
-        <AuthForm
-            formType="login" onSubmit={handleSubmit} />
-    )
+        <AuthForm formType="register" onSubmit={handleSubmit} />
+    );
 }
