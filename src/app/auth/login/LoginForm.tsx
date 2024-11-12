7b3d087d2fb449664113446ef5fa005b86f0763f
Marcus <moliveira@pinterest.com>
2024-11-12 02:40:47 -0800

    feat: :sparkles: Login integration with backend

--- src/app/auth/login/LoginForm.tsx
+++ src/app/auth/login/LoginForm.tsx
@@ -1,11 +1,43 @@
 'use client';
 import { AuthForm } from "@/app/components/AuthForm";
 import { InputField } from "../../components/InputField";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+type ServerError = {
+    message: string;
+}
 
 export default function LoginForm() {
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const [errors, setErrors] = useState<string[]>([]);
+    const router = useRouter();
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log('submit');
+
+        const formData = new FormData(e.currentTarget);
+        const email = formData.get("email");
+        const password = formData.get("password");
+
+        try {
+            const response = await fetch('/auth/login/api', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ email, password }),
+            });
+
+            if (response.ok) {
+                router.push('/');
+                return;
+            }
+
+            const payload: ServerError[] = await response.json();
+            setErrors(payload.map((error) => error.message));
+
+        } catch (error) {
+            console.log(error);
+            setErrors(['An unknown error occured.'])
+        }
     }
 
     return (
